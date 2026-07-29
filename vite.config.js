3e0d0c95c8162cf81ac7449085dcc24461ebb754
patisonkindle-commits <patiosn.kindle@gmail.com>
2026-07-29 23:04:38 +0700

    Fix terser issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: (id) => {
